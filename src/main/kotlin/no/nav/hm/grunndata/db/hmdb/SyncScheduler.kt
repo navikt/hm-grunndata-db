dea93073689af6386eb8350512293bbaa8f14e76
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2022-11-11 10:23:18 +0100

    added sync supplier scheduler

--- src/main/kotlin/no/nav/hm/grunndata/db/hmdb/SyncScheduler.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/hmdb/SyncScheduler.kt
@@ -16,20 +16,24 @@ class SyncScheduler(private val hmDbClient: HmDbClient, private val hmdbBatchRep
         private val LOG = LoggerFactory.getLogger(SyncScheduler::class.java)
     }
 
-    @Scheduled(cron = "0 30 * * * *")
+    @Scheduled(cron = "0 10 * * * *")
     fun syncSuppliers() {
-        LOG.info("Running sync suppliers")
-        val syncSupplier = hmdbBatchRepository.findByName(SYNC_SUPPLIERS) ?:
+        val syncBatchJob = hmdbBatchRepository.findByName(SYNC_SUPPLIERS) ?:
             hmdbBatchRepository.save(HmDbBatch(name= SYNC_SUPPLIERS,
-                syncfrom = LocalDateTime.now().minusYears(20).truncatedTo(ChronoUnit.SECONDS)))
-        val suppliers = hmDbClient.fetchSuppliers(syncSupplier.syncfrom)
-        val entities = suppliers.map { it.toSupplier() }
+                syncfrom = LocalDateTime.now().minusYears(100).truncatedTo(ChronoUnit.SECONDS)))
+        val suppliers = hmDbClient.fetchSuppliers(syncBatchJob.syncfrom)
+        LOG.info("Calling supplier sync from ${syncBatchJob.syncfrom}, Got total of ${suppliers.size} suppliers")
+        val entities = suppliers.map { it.toSupplier() }.sortedBy { it.updated }
         runBlocking {
             entities.forEach {
-                val supplier = supplierRepository.findByIdentifier(it.identifier)
-
+                val saved = supplierRepository.findByIdentifier(it.identifier)?.let { inDb->
+                    supplierRepository.update(it.copy(id=inDb.id, created = inDb.created))
+                } ?: run {
+                    supplierRepository.save(it)
+                }
+                LOG.info("saved supplier ${saved.id} with identifier ${saved.identifier} and lastupdated ${saved.updated}")
             }
+            hmdbBatchRepository.update(syncBatchJob.copy(syncfrom = suppliers.last().lastupdated))
         }
-
     }
 }
