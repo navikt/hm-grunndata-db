9a8ad0a9e8c97d800701a4b2d7165ff653166664
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-02-06 13:39:40 +0100

    debug log

--- src/main/kotlin/no/nav/hm/grunndata/db/product/AttributeTagService.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/product/AttributeTagService.kt
@@ -1,14 +1,20 @@
 package no.nav.hm.grunndata.db.product
 
 import jakarta.inject.Singleton
+import org.slf4j.LoggerFactory
 
 @Singleton
 class AttributeTagService(private val bestillingsordning: Bestillingsordning) {
 
+    companion object {
+        private val LOG = LoggerFactory.getLogger(AttributeTagService::class.java)
+    }
     fun addBestillingsordningAttribute(entity: Product): Product =
         entity.HMSArtNr?.let {
-            if (bestillingsordning.isBestillingsordning(entity.HMSArtNr))
+            if (bestillingsordning.isBestillingsordning(entity.HMSArtNr)) {
+                LOG.debug("Got product in bestillingsordning ${entity.HMSArtNr}")
                 entity.copy(attributes = entity.attributes.plus(AttributeNames.bestillingsordning to true))
+            }
             else
                 entity.copy(attributes = entity.attributes.filterNot { it.key == AttributeNames.bestillingsordning })
         } ?: entity
