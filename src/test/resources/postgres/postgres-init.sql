b37d020ac68fbd307230f926dcc32072f065af1f
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-03-03 08:28:44 +0100

    put register in same db

--- src/test/resources/postgres/postgres-init.sql
+++ src/test/resources/postgres/postgres-init.sql
@@ -2,3 +2,5 @@ CREATE USER gdb with password 'gdb';
 CREATE DATABASE gdb owner gdb;
 CREATE USER register with password 'register';
 CREATE DATABASE register owner register;
+CREATE USER media with password 'media';
+CREATE DATABASE media owner media;
