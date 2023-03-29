93f8fcfe9e35d56bf5b0da003eb2b026e751ed81
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-03-29 12:26:34 +0200

    use rapidapp name for createdBy

--- src/main/kotlin/no/nav/hm/grunndata/db/GdbRapidPushService.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/GdbRapidPushService.kt
@@ -3,6 +3,7 @@ package no.nav.hm.grunndata.db
 import jakarta.inject.Singleton
 import no.nav.hm.grunndata.rapid.dto.RapidDTO
 import no.nav.hm.grunndata.rapid.dto.rapidDTOVersion
+import no.nav.hm.grunndata.rapid.event.RapidApp
 import no.nav.hm.rapids_rivers.micronaut.RapidPushService
 import java.time.LocalDateTime
 
@@ -12,7 +13,7 @@ class GdbRapidPushService(private val kafkaRapidService: RapidPushService) {
     fun pushDTOToKafka(dto: RapidDTO, eventName: String) {
         kafkaRapidService.pushToRapid(
             key = "$eventName-${dto.id}",
-            eventName = eventName, payload = dto, keyValues = mapOf("createdBy" to GDB,
+            eventName = eventName, payload = dto, keyValues = mapOf("createdBy" to RapidApp.grunndata_db,
                 "dtoVersion" to rapidDTOVersion)
         )
     }
