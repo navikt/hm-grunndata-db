e2e888f271ad6872d762df33b87df47db064ea83
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-04-04 13:43:20 +0200

    upgrade all libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,19 +3,19 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 val jvmTarget = "17"
-val micronautVersion="3.8.6"
+val micronautVersion="3.8.7"
 val kafkaVersion = "3.2.1"
 val micrometerRegistryPrometheusVersion = "1.9.1"
 val junitJupiterVersion = "5.9.0"
 val jacksonVersion = "2.13.4"
-val logbackClassicVersion = "1.2.11"
-val logbackEncoderVersion = "7.2"
-val postgresqlVersion= "42.3.3"
-val tcVersion= "1.16.3"
-val mockkVersion = "1.13.2"
-val kotestVersion = "5.5.0"
+val logbackClassicVersion = "1.4.6"
+val logbackEncoderVersion = "7.3"
+val postgresqlVersion= "42.5.4"
+val tcVersion= "1.17.6"
+val mockkVersion = "1.13.4"
+val kotestVersion = "5.5.5"
 val rapidsRiversVersion = "202303011052"
-val grunndataDtoVersion = "202303311459"
+val grunndataDtoVersion = "202304031307"
 
 group = "no.nav.hm"
 version = properties["version"] ?: "local-build"
@@ -25,7 +25,7 @@ plugins {
     kotlin("kapt") version "1.7.0"
     id("java")
     id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("io.micronaut.application") version "3.7.2"
+    id("io.micronaut.application") version "3.7.4"
 }
 
 configurations.all {
@@ -47,7 +47,7 @@ dependencies {
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("io.micronaut.sql:micronaut-jdbc-hikari")
-    annotationProcessor("io.micronaut.data:micronaut-data-processor")
+    kapt("io.micronaut.data:micronaut-data-processor")
     implementation("org.postgresql:postgresql:${postgresqlVersion}")
     implementation("io.micronaut.flyway:micronaut-flyway")
     implementation("io.micronaut:micronaut-runtime")
@@ -69,7 +69,7 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
     testImplementation("org.testcontainers:postgresql:${tcVersion}")
 }
 
