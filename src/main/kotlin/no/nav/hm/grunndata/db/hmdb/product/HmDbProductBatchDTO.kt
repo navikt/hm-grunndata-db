4c1b74ef9f3e63958999be1249b57fe21a0bc5a3
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-04-14 12:51:02 +0200

    remove changeTo

--- src/main/kotlin/no/nav/hm/grunndata/db/hmdb/product/HmDbProductBatchDTO.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/hmdb/product/HmDbProductBatchDTO.kt
@@ -3,7 +3,6 @@ package no.nav.hm.grunndata.db.hmdb.product
 import java.time.LocalDateTime
 
 class HmDbProductBatchDTO(
-    val changeTo: LocalDateTime,
     val products: List<HmDbProductDTO>,
     val blobs: Map<Long, List<BlobDTO>>,
     val techdata: Map<Long, List<TechDataDTO>>
