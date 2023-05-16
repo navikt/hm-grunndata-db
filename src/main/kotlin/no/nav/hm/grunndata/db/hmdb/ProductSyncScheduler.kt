069bedfd9216c211cfaa72dbcad8706c5f52620b
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-05-16 12:46:58 +0200

    scheduler for sync deleted products

--- src/main/kotlin/no/nav/hm/grunndata/db/hmdb/ProductSyncScheduler.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/hmdb/ProductSyncScheduler.kt
@@ -38,4 +38,22 @@ open class ProductSyncScheduler(private val productSync: ProductSync,
         }
     }
 
+    @Scheduled(fixedDelay = "30m")
+    fun syncActiveIds() {
+        if (leaderElection.isLeader()) {
+            if (stopped) {
+                LOG.warn("scheduler is stopped, maybe because of uncaught errors!")
+                return
+            }
+            runBlocking {
+                try {
+                    productSync.syncDeletedProductIds()
+                } catch (e: Exception) {
+                    LOG.error("Got uncaught exception while run product deleted sync, stop scheduler", e)
+                    stopped = true
+                }
+            }
+        }
+    }
+
 }
