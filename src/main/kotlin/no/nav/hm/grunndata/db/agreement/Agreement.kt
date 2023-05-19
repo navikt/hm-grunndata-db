f84829c6b36e74feaf352be8da625d7b07b34c49
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-05-19 11:38:16 +0200

    added support for status

--- src/main/kotlin/no/nav/hm/grunndata/db/agreement/Agreement.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/agreement/Agreement.kt
@@ -11,12 +11,15 @@ import no.nav.hm.grunndata.rapid.dto.AgreementPost
 import java.time.LocalDateTime
 import java.util.*
 
+
+
 @MappedEntity("agreement_v1")
 data class Agreement (
     @field:Id
     val id: UUID = UUID.randomUUID(),
     val identifier: String,
     val title: String,
+    val status: AgreementStatus = AgreementStatus.ACTIVE,
     val resume: String?,
     val text: String?,
     val reference: String,
@@ -31,6 +34,10 @@ data class Agreement (
     val created: LocalDateTime = LocalDateTime.now(),
     val updated: LocalDateTime = LocalDateTime.now()
 )
+
+enum class AgreementStatus {
+    ACTIVE, INACTIVE, DELETED
+}
 
 fun Agreement.toDTO(): AgreementDTO = AgreementDTO(
     id = id, identifier = identifier, title = title, resume = resume, text = text, reference=reference,
