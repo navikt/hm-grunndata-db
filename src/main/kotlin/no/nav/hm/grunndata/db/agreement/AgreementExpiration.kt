a83f26d3799753fde1dc86424511f9b17fde7397
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-06-02 09:51:37 +0200

    more logs

--- src/main/kotlin/no/nav/hm/grunndata/db/agreement/AgreementExpiration.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/agreement/AgreementExpiration.kt
@@ -4,6 +4,7 @@ import jakarta.inject.Singleton
 import no.nav.hm.grunndata.db.product.ProductService
 import no.nav.hm.grunndata.rapid.dto.AgreementStatus
 import no.nav.hm.grunndata.rapid.event.EventName
+import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
 import javax.transaction.Transactional
 
@@ -11,7 +12,11 @@ import javax.transaction.Transactional
 open class AgreementExpiration(private val agreementService: AgreementService,
                                private val productService: ProductService) {
 
-    private val updatedBy = "AGREEMENTEXPIRATION"
+    companion object {
+        private const val updatedBy = "AGREEMENTEXPIRATION"
+        private val LOG = LoggerFactory.getLogger(AgreementExpiration::class.java)
+
+    }
     suspend fun expiredAgreements() {
         val expiredList = agreementService.findByStatusAndExpiredBefore(AgreementStatus.ACTIVE)
         expiredList.forEach {
@@ -21,10 +26,12 @@ open class AgreementExpiration(private val agreementService: AgreementService,
 
     @Transactional
     open suspend fun deactiveProductsInAgreement(agreement: Agreement) {
-        agreementService.save(agreement.copy(status = AgreementStatus.INACTIVE,
+        LOG.info("Agreement ${agreement.id} ${agreement.reference} has expired")
+        agreementService.update(agreement.copy(status = AgreementStatus.INACTIVE,
             updated = LocalDateTime.now(), updatedBy = updatedBy))
         val productsInAgreement = productService.findByAgreementId(agreement.id)
         productsInAgreement.forEach { product ->
+            LOG.info("Found ${product.id} in expired agreement")
             val notExpired = product.agreements?.filterNot {
                 it.id == agreement.id
             }
