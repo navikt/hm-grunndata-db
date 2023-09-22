a2606eacdf25b1e7cacb80dd6f7d3900845432a6
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-09-22 10:38:13 +0200

    added series controller

---
+++ src/main/kotlin/no/nav/hm/grunndata/db/series/SeriesController.kt
@@ -0,0 +1,24 @@
+package no.nav.hm.grunndata.db.series
+
+import io.micronaut.data.model.Page
+import io.micronaut.data.model.Pageable
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
+import no.nav.hm.grunndata.rapid.dto.SeriesRapidDTO
+import org.slf4j.LoggerFactory
+
+@Controller(SeriesController.API_V1_SERIES)
+class SeriesController(private val seriesSeries: SeriesService) {
+
+    companion object {
+        const val API_V1_SERIES = "/api/v1/series"
+        private val LOG = LoggerFactory.getLogger(SeriesController::class.java)
+    }
+
+    @Get("/{?params*}")
+    suspend fun findSeries(@QueryValue params: Map<String, String>?,
+                             pageable: Pageable
+    ): Page<SeriesRapidDTO> = seriesSeries.findSeries(params, pageable)
+
+}
