2caea4f34a7d9bec1b2e069aef2bdddf808d6af2
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-12-14 11:26:38 +0100

    disable agreement expiration functions, moved to register

--- src/main/kotlin/no/nav/hm/grunndata/db/agreement/AgreementExpiration.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/agreement/AgreementExpiration.kt
@@ -13,35 +13,35 @@ import java.time.LocalDateTime
 open class AgreementExpiration(private val agreementService: AgreementService,
                                private val productService: ProductService) {
 
-    companion object {
-        private const val expiration = "AGREEMENTEXPIRATION"
-        private val LOG = LoggerFactory.getLogger(AgreementExpiration::class.java)
-
-    }
-    suspend fun expiredAgreements() {
-        val expiredList = agreementService.findByStatusAndExpiredBefore(AgreementStatus.ACTIVE)
-        expiredList.forEach {
-            deactiveProductsInExpiredAgreement(it)
-        }
-    }
-
-    @Transactional
-    open suspend fun deactiveProductsInExpiredAgreement(expiredAgreement: Agreement) {
-        LOG.info("Agreement ${expiredAgreement.id} ${expiredAgreement.reference} has expired")
-        agreementService.saveAndPushTokafka(agreement = expiredAgreement.copy(status = AgreementStatus.INACTIVE,
-            updated = LocalDateTime.now(), updatedBy = expiration), eventName = EventName.expiredAgreementV1)
-        val productsInAgreement = productService.findByAgreementId(expiredAgreement.id)
-        productsInAgreement.forEach { product ->
-            LOG.info("Found product: ${product.id} in expired agreement")
-            val expiredProductAgreements = product.agreements?.filter {
-                it.id == expiredAgreement.id
-            }?.toSet()
-            val notExpired = product.agreements?.filterNot {
-                it.id == expiredAgreement.id
-            }?.toSet()
-            productService.saveAndPushTokafka(product.copy(agreements = notExpired,
-                pastAgreements = product.pastAgreements.plus(expiredProductAgreements) as Set<ProductAgreement>,
-                updated = LocalDateTime.now(), updatedBy = expiration), eventName = EventName.expiredProductAgreementV1)
-        }
-    }
+//    companion object {
+//        private const val expiration = "AGREEMENTEXPIRATION"
+//        private val LOG = LoggerFactory.getLogger(AgreementExpiration::class.java)
+//
+//    }
+//    suspend fun expiredAgreements() {
+//        val expiredList = agreementService.findByStatusAndExpiredBefore(AgreementStatus.ACTIVE)
+//        expiredList.forEach {
+//            deactiveProductsInExpiredAgreement(it)
+//        }
+//    }
+//
+//    @Transactional
+//    open suspend fun deactiveProductsInExpiredAgreement(expiredAgreement: Agreement) {
+//        LOG.info("Agreement ${expiredAgreement.id} ${expiredAgreement.reference} has expired")
+//        agreementService.saveAndPushTokafka(agreement = expiredAgreement.copy(status = AgreementStatus.INACTIVE,
+//            updated = LocalDateTime.now(), updatedBy = expiration), eventName = EventName.expiredAgreementV1)
+//        val productsInAgreement = productService.findByAgreementId(expiredAgreement.id)
+//        productsInAgreement.forEach { product ->
+//            LOG.info("Found product: ${product.id} in expired agreement")
+//            val expiredProductAgreements = product.agreements?.filter {
+//                it.id == expiredAgreement.id
+//            }?.toSet()
+//            val notExpired = product.agreements?.filterNot {
+//                it.id == expiredAgreement.id
+//            }?.toSet()
+//            productService.saveAndPushTokafka(product.copy(agreements = notExpired,
+//                pastAgreements = product.pastAgreements.plus(expiredProductAgreements) as Set<ProductAgreement>,
+//                updated = LocalDateTime.now(), updatedBy = expiration), eventName = EventName.expiredProductAgreementV1)
+//        }
+//    }
 }
