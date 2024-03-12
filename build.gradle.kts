e28758e2d584d1c354dda976b7d3a8b10e6718dd
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-03-12 09:15:40 +0100

    river for sync product agreements

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ val tcVersion = "1.17.6"
 val mockkVersion = "1.13.4"
 val kotestVersion = "5.5.5"
 val rapidsRiversVersion = "202401101532"
-val grunndataDtoVersion = "202403071208"
+val grunndataDtoVersion = "202403110951"
 
 group = "no.nav.hm"
 version = properties["version"] ?: "local-build"
---
+++ src/main/kotlin/no/nav/hm/grunndata/db/product/ProductAgreementRegistrationRiver.kt
@@ -0,0 +1,92 @@
+package no.nav.hm.grunndata.db.product
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.micronaut.context.annotation.Context
+import io.micronaut.context.annotation.Requires
+import kotlinx.coroutines.runBlocking
+import no.nav.helse.rapids_rivers.*
+import no.nav.hm.grunndata.db.agreement.AgreementService
+import no.nav.hm.grunndata.rapid.dto.*
+import no.nav.hm.grunndata.rapid.event.EventName
+import no.nav.hm.rapids_rivers.micronaut.RiverHead
+import org.slf4j.LoggerFactory
+
+@Context
+@Requires(bean = KafkaRapid::class)
+class ProductAgreementRegistrationRiver(
+    river: RiverHead,
+    private val objectMapper: ObjectMapper,
+    private val agreementService: AgreementService,
+    private val productService: ProductService
+) : River.PacketListener {
+    companion object {
+        private val LOG = LoggerFactory.getLogger(ProductAgreementRegistrationRiver::class.java)
+    }
+
+    init {
+        LOG.info("Using rapid dto version: $rapidDTOVersion")
+        river
+            .validate { it.demandValue("eventName", EventName.registeredProductAgreementV1) }
+            .validate { it.demandKey("payload") }
+            .validate { it.demandKey("eventId") }
+            .validate { it.demandKey("dtoVersion") }
+            .validate { it.demandKey("createdTime") }
+            .register(this)
+    }
+
+    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        val eventId = packet["eventId"].asText()
+        val dtoVersion = packet["dtoVersion"].asLong()
+        val createdTime = packet["createdTime"].asLocalDateTime()
+        if (dtoVersion > rapidDTOVersion) LOG.warn("dto version $dtoVersion is newer than $rapidDTOVersion")
+        val dto = objectMapper.treeToValue(packet["payload"], ProductAgreementRegistrationRapidDTO::class.java)
+        LOG.info(
+            "got product agreement registration with supplierId: ${dto.supplierId} and supplierRef: ${dto.supplierRef}" +
+                    "eventId $eventId eventTime: $createdTime"
+        )
+        runBlocking {
+            productService.findBySupplierIdAndSupplierRef(dto.supplierId, dto.supplierRef)?.let { inDb ->
+                val product = mergeAgreementInProduct(inDb, dto)
+                productService.saveAndPushTokafka(product.toEntity(), EventName.syncedRegisterProductV1)
+            }
+        }
+
+    }
+
+    private suspend fun mergeAgreementInProduct(
+        product: ProductRapidDTO,
+        agreement: ProductAgreementRegistrationRapidDTO
+    ): ProductRapidDTO {
+
+        val filteredAgreements = product.agreements.filter { it.postId != agreement.postId }
+        if (agreement.status == ProductAgreementStatus.DELETED) return product.copy(agreements = filteredAgreements)
+
+        val hmsNr = if (product.hmsArtNr != agreement.hmsArtNr) {
+            LOG.info("This product ${product.id} has a different hmsArtNr than the agreement ${agreement.hmsArtNr}")
+            agreement.hmsArtNr
+        } else product.hmsArtNr
+
+        val updated = agreementService.findById(agreement.agreementId)?.let { agreementInDb ->
+            val foundPost = agreementInDb.posts.find { post -> post.id == agreement.postId }
+                ?: throw IllegalStateException("Post ${agreement.postId} not found in agreement ${agreement.agreementId}")
+            AgreementInfo(
+                id = agreementInDb.id,
+                identifier = agreementInDb.identifier,
+                title = agreementInDb.title,
+                rank = agreement.rank,
+                postNr = agreement.post,
+                postIdentifier = foundPost.identifier,
+                postTitle =foundPost.title,
+                postId = agreement.postId,
+                refNr = foundPost.refNr,
+                reference = agreement.reference,
+                expired = agreement.expired,
+                published = agreementInDb.published
+            )
+        } ?: throw IllegalStateException("Agreement ${agreement.agreementId} not found, that can not be possible check if agreements are in sync")
+        return product.copy(
+            hmsArtNr = hmsNr,
+            agreements = filteredAgreements + updated
+        )
+    }
+}
