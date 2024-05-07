06947317167bcb6acbbc7cc0b18b66a7d5b71cbd
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2024-05-07 10:55:16 +0200

    Revert "Added test endpoints, logging and reduced cache duration for simpler testing in dev"
    This reverts commit a291d5a67518ad3951b3f18fa7a6e72415c3544c.

--- src/main/kotlin/no/nav/hm/grunndata/db/internal/AliveController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/internal/AliveController.kt
@@ -2,16 +2,13 @@ package no.nav.hm.grunndata.db.internal
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Post
 import no.nav.hm.grunndata.db.hmdb.product.ProductSyncScheduler
-import no.nav.hm.grunndata.db.product.DigihotSortiment
 
 @Controller("/internal")
-class AliveController(private val productSyncScheduler: ProductSyncScheduler, private val digihotSortiment: DigihotSortiment) {
+class AliveController(private val productSyncScheduler: ProductSyncScheduler) {
     companion object {
         private val LOG = org.slf4j.LoggerFactory.getLogger(AliveController::class.java)
     }
-
     @Get("/isAlive")
     fun alive(): String {
         if (productSyncScheduler.isStopped()) {
@@ -24,23 +21,4 @@ class AliveController(private val productSyncScheduler: ProductSyncScheduler, pr
     @Get("/isReady")
     fun ready() = "OK"
 
-    // FIXME: Remove again
-    @Post("/test1")
-    fun test1()  = digihotSortiment.cachedBestillingsordning()
-
-    // FIXME: Remove again
-    @Post("/test2")
-    fun test2()  = digihotSortiment.cachedDigitalSoknad()
-
-    // FIXME: Remove again
-    @Post("/test3")
-    fun test3()  = digihotSortiment.cachedPakrevdGodkjenningskurs()
-
-    // FIXME: Remove again
-    @Post("/test4")
-    fun test4()  = digihotSortiment.cachedProdukttype()
-
-    // FIXME: Remove again
-    @Post("/test5")
-    fun test5()  = digihotSortiment.cachedIsoMetadata()
 }
