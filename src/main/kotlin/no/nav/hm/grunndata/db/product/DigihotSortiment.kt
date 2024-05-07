a3280341219320f282e022dd0b0cb3664da197bf
Kristoffer A. Iversen <kristoffer.a.iversen@gmail.com>
2024-05-07 12:56:05 +0200

    Fiks cache av github sortimenter (#7)
    * Revert "Revert "Use 1h caching of digihot-sortiment values rather than remembering until next restart of the pods""
    
    This reverts commit b4544621235f06279797577ef802c801a9122c1a.
    
    * Divide caches up into separate caching buckets
    
    * Dev deploy from feature branch
    
    * Added test endpoints, logging and reduced cache duration for simpler testing in dev
    
    * Revert "Added test endpoints, logging and reduced cache duration for simpler testing in dev"
    
    This reverts commit a291d5a67518ad3951b3f18fa7a6e72415c3544c.

--- src/main/kotlin/no/nav/hm/grunndata/db/product/DigihotSortiment.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/product/DigihotSortiment.kt
@@ -2,6 +2,8 @@ package no.nav.hm.grunndata.db.product
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.micronaut.cache.annotation.CacheConfig
+import io.micronaut.cache.annotation.Cacheable
 import io.micronaut.context.annotation.Value
 import jakarta.inject.Singleton
 import no.nav.hm.grunndata.rapid.dto.PakrevdGodkjenningskurs
@@ -11,7 +13,7 @@ import java.net.URI
 import java.util.UUID
 
 @Singleton
-class DigihotSortiment(
+open class DigihotSortiment(
     @Value("\${digihotSortiment.bestillingsordning}")
     private val bestillingsordningUrl : String,
     @Value("\${digihotSortiment.digitalSoknad}")
@@ -28,10 +30,38 @@ class DigihotSortiment(
         private val LOG = LoggerFactory.getLogger(AttributeTagService::class.java)
     }
 
-    private val bestillingsordningMap: Map<String, BestillingsordningDTO> =
-        objectMapper.readValue(URI(bestillingsordningUrl).toURL(), object : TypeReference<List<BestillingsordningDTO>>(){}).associateBy { it.hmsnr }
+    fun isBestillingsordning(hmsnr: String): Boolean = cachedBestillingsordning().containsKey(hmsnr)
 
-    private val digitalSoknadMap: List<SortimentDTO> =
+    fun getPostIdInDigitalCatalog(postId: UUID): Boolean {
+        return cachedDigitalSoknad().any { it.postIds.contains(postId) }
+    }
+
+    fun getSortimentKategoriByPostIdInDigitalCatalog(postId: UUID): String? {
+        return cachedDigitalSoknad().find { it.postIds.contains(postId) }?.sortimentKategori
+    }
+
+    fun getPakrevdGodkjenningskurs(isocode: String): PakrevdGodkjenningskurs? {
+        val relevantIsoCodePrefix = isocode.take(6)
+        return cachedPakrevdGodkjenningskurs()[relevantIsoCodePrefix]
+    }
+
+    fun getProdukttype(isocode: String): Produkttype? {
+        val relevantIsoCodePrefix = isocode.take(6)
+        return cachedProdukttype()[relevantIsoCodePrefix]?.produkttype?.let { Produkttype.valueOf(it) }
+    }
+
+    fun getIsoMetadata(isocode: String): IsoMetadataDTO? {
+        val relevantIsoCodePrefix = isocode.take(8)
+        return cachedIsoMetadata()[relevantIsoCodePrefix]
+    }
+
+    @Cacheable("digihot-sortiment-bestillingsordning")
+    open fun cachedBestillingsordning(): Map<String, BestillingsordningDTO> = objectMapper
+        .readValue(URI(bestillingsordningUrl).toURL(), object : TypeReference<List<BestillingsordningDTO>>(){})
+        .associateBy { it.hmsnr }
+
+    @Cacheable("digihot-sortiment-digitalsoknad")
+    open fun cachedDigitalSoknad(): List<SortimentDTO> =
         objectMapper.readTree(URI(digitalSoknadUrl).toURL()).let { node ->
             require(node.isObject) { "unexpected non-object reply from digihot-sortiment" }
             val res = mutableListOf<SortimentDTO>()
@@ -47,41 +77,17 @@ class DigihotSortiment(
             res
         }
 
-    private val pakrevdGodkjenningskursMap: Map<String, PakrevdGodkjenningskurs> =
+    @Cacheable("digihot-sortiment-paakrevdgodkjenningskurs")
+    open fun cachedPakrevdGodkjenningskurs(): Map<String, PakrevdGodkjenningskurs> =
         objectMapper.readValue(URI(pakrevdGodkjenningskursUrl).toURL(), object : TypeReference<List<PakrevdGodkjenningskurs>>(){}).associateBy { it.isokode }
 
-    private val produkttypeMap: Map<String, ProdukttypeDTO> =
+    @Cacheable("digihot-sortiment-produkttype")
+    open fun cachedProdukttype(): Map<String, ProdukttypeDTO> =
         objectMapper.readValue(URI(produkttypeUrl).toURL(), object : TypeReference<List<ProdukttypeDTO>>(){}).associateBy { it.isokode }
 
-    private val isoMetadataMap: Map<String, IsoMetadataDTO> =
+    @Cacheable("digihot-sortiment-isometadata")
+    open fun cachedIsoMetadata(): Map<String, IsoMetadataDTO> =
         objectMapper.readValue(URI(isoMetadataUrl).toURL(), object : TypeReference<List<IsoMetadataDTO>>(){}).associateBy { it.isokode }
-
-    fun isBestillingsordning(hmsnr: String): Boolean = bestillingsordningMap.containsKey(hmsnr)
-
-    fun getBestillingsorning(hmsnr: String): BestillingsordningDTO? = bestillingsordningMap[hmsnr]
-
-    fun getPostIdInDigitalCatalog(postId: UUID): Boolean {
-        return digitalSoknadMap.any { it.postIds.contains(postId) }
-    }
-
-    fun getSortimentKategoriByPostIdInDigitalCatalog(postId: UUID): String? {
-        return digitalSoknadMap.find { it.postIds.contains(postId) }?.sortimentKategori
-    }
-
-    fun getPakrevdGodkjenningskurs(isocode: String): PakrevdGodkjenningskurs? {
-        val relevantIsoCodePrefix = isocode.take(6)
-        return pakrevdGodkjenningskursMap[relevantIsoCodePrefix]
-    }
-
-    fun getProdukttype(isocode: String): Produkttype? {
-        val relevantIsoCodePrefix = isocode.take(6)
-        return produkttypeMap[relevantIsoCodePrefix]?.produkttype?.let { Produkttype.valueOf(it) }
-    }
-
-    fun getIsoMetadata(isocode: String): IsoMetadataDTO? {
-        val relevantIsoCodePrefix = isocode.take(8)
-        return isoMetadataMap[relevantIsoCodePrefix]
-    }
 }
 
 data class BestillingsordningDTO(
