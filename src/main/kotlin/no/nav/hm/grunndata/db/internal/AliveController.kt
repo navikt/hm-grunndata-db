da39efc892c0a5c8e5c57ec64581ed362848aceb
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-05-23 12:26:51 +0200

    restart if consumer is down

--- src/main/kotlin/no/nav/hm/grunndata/db/internal/AliveController.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/internal/AliveController.kt
@@ -2,10 +2,11 @@ package no.nav.hm.grunndata.db.internal
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import no.nav.helse.rapids_rivers.KafkaRapid
 import no.nav.hm.grunndata.db.hmdb.product.ProductSyncScheduler
 
 @Controller("/internal")
-class AliveController(private val productSyncScheduler: ProductSyncScheduler) {
+class AliveController(private val productSyncScheduler: ProductSyncScheduler, private val kafkaRapid: KafkaRapid) {
     companion object {
         private val LOG = org.slf4j.LoggerFactory.getLogger(AliveController::class.java)
     }
@@ -14,6 +15,10 @@ class AliveController(private val productSyncScheduler: ProductSyncScheduler) {
         if (productSyncScheduler.isStopped()) {
             LOG.error("ProductSyncScheduler is stopped")
             throw IllegalStateException("ProductSyncScheduler is stopped")
+        }
+        if (kafkaRapid.isConsumerClosed()) {
+            LOG.error("Kafka consumer is closed")
+            throw IllegalStateException("Kafka consumer is closed")
         }
         return "ALIVE"
     }
