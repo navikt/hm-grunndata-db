e01ff5783cb083631b762a387b345b7e242f2ee7
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-06-19 15:10:20 +0200

    fix typo

--- src/main/kotlin/no/nav/hm/grunndata/db/product/ProductService.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/product/ProductService.kt
@@ -115,7 +115,7 @@ open class ProductService(
             if (params.contains("supplierId"))  root[Product::supplierId] eq UUID.fromString(params["supplierId"]!!)
             if (params.contains("updated")) root[Product::updated] greaterThanOrEqualTo LocalDateTime.parse(params["updated"])
             if (params.contains("status")) root[Product::status] eq params["status"]
-            if (params.contains("seriesUUUID")) root[Product::seriesUUID] eq UUID.fromString(params["seriesUUUID"]!!)
+            if (params.contains("seriesUUID")) root[Product::seriesUUID] eq UUID.fromString(params["seriesUUID"]!!)
         }
     }
 
