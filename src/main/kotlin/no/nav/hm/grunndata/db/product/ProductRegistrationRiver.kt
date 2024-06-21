1ed14233507a929856db31d7ae520f1faedd26d3
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-06-21 13:55:52 +0200

    Skip event

--- src/main/kotlin/no/nav/hm/grunndata/db/product/ProductRegistrationRiver.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/product/ProductRegistrationRiver.kt
@@ -4,10 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import io.micronaut.context.annotation.Context
 import io.micronaut.context.annotation.Requires
 import kotlinx.coroutines.runBlocking
-import no.nav.helse.rapids_rivers.*
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.KafkaRapid
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.River
+import no.nav.helse.rapids_rivers.asLocalDateTime
 import no.nav.hm.grunndata.db.series.Series
 import no.nav.hm.grunndata.db.series.SeriesService
-import no.nav.hm.grunndata.rapid.dto.*
+import no.nav.hm.grunndata.rapid.dto.AdminStatus
+import no.nav.hm.grunndata.rapid.dto.DraftStatus
+import no.nav.hm.grunndata.rapid.dto.ProductRegistrationRapidDTO
+import no.nav.hm.grunndata.rapid.dto.SeriesData
+import no.nav.hm.grunndata.rapid.dto.rapidDTOVersion
 import no.nav.hm.grunndata.rapid.event.EventName
 import no.nav.hm.rapids_rivers.micronaut.RiverHead
 import org.slf4j.LoggerFactory
@@ -48,41 +56,47 @@ class ProductRegistrationRiver(
                     "createdBy: ${dto.createdBy} identifier: ${dto.productDTO.identifier}"
         )
         runBlocking {
-            if (dto.adminStatus == AdminStatus.APPROVED && dto.draftStatus == DraftStatus.DONE) {
-                // series and products need to be merged before sending down the river
-                val riverProduct = dto.productDTO.toEntity()
-                seriesService.findById(riverProduct.seriesUUID!!)?.let { series ->
-                    val mergedProduct = riverProduct.copy(
-                        title = series.title,
-                        attributes = riverProduct.attributes.copy(
-                            text = series.text,
-                            url = series.seriesData?.attributes?.url,
-                            keywords = series.seriesData?.attributes?.keywords?.toList()
-                        ),
-                        isoCategory = series.isoCategory,
-                        seriesIdentifier = series.identifier,
-                        media = series.seriesData?.media ?: riverProduct.media,
-                    )
-                    productService.saveAndPushTokafka(mergedProduct, EventName.syncedRegisterProductV1)
-                } ?: run {
-                    LOG.warn("Series not found ${riverProduct.seriesUUID}, saving series for product ${riverProduct.id}")
-                    seriesService.saveAndPushTokafka(
-                        Series(
-                            id = riverProduct.seriesUUID,
-                            supplierId = riverProduct.supplierId,
-                            identifier = riverProduct.seriesIdentifier?:riverProduct.seriesUUID.toString(),
-                            title = riverProduct.title,
-                            text = riverProduct.attributes.text?:"",
-                            isoCategory = riverProduct.isoCategory,
-                            seriesData = SeriesData(media = riverProduct.media),
-                            createdBy = riverProduct.createdBy,
-                            updatedBy = riverProduct.updatedBy,
-                            created = riverProduct.created,
-                            updated = riverProduct.updated
-                        ), eventName = EventName.syncedRegisterSeriesV1
-                    )
-                    productService.saveAndPushTokafka(riverProduct, EventName.syncedRegisterProductV1)
+            try {
+
+
+                if (dto.adminStatus == AdminStatus.APPROVED && dto.draftStatus == DraftStatus.DONE) {
+                    // series and products need to be merged before sending down the river
+                    val riverProduct = dto.productDTO.toEntity()
+                    seriesService.findById(riverProduct.seriesUUID!!)?.let { series ->
+                        val mergedProduct = riverProduct.copy(
+                            title = series.title,
+                            attributes = riverProduct.attributes.copy(
+                                text = series.text,
+                                url = series.seriesData?.attributes?.url,
+                                keywords = series.seriesData?.attributes?.keywords?.toList()
+                            ),
+                            isoCategory = series.isoCategory,
+                            seriesIdentifier = series.identifier,
+                            media = series.seriesData?.media ?: riverProduct.media,
+                        )
+                        productService.saveAndPushTokafka(mergedProduct, EventName.syncedRegisterProductV1)
+                    } ?: run {
+                        LOG.warn("Series not found ${riverProduct.seriesUUID}, saving series for product ${riverProduct.id}")
+                        seriesService.saveAndPushTokafka(
+                            Series(
+                                id = riverProduct.seriesUUID,
+                                supplierId = riverProduct.supplierId,
+                                identifier = riverProduct.seriesIdentifier ?: riverProduct.seriesUUID.toString(),
+                                title = riverProduct.title,
+                                text = riverProduct.attributes.text ?: "",
+                                isoCategory = riverProduct.isoCategory,
+                                seriesData = SeriesData(media = riverProduct.media),
+                                createdBy = riverProduct.createdBy,
+                                updatedBy = riverProduct.updatedBy,
+                                created = riverProduct.created,
+                                updated = riverProduct.updated
+                            ), eventName = EventName.syncedRegisterSeriesV1
+                        )
+                        productService.saveAndPushTokafka(riverProduct, EventName.syncedRegisterProductV1)
+                    }
                 }
+            } catch (e: Exception) {
+                LOG.error("Skipping erroneous event", e)
             }
         }
     }
