9b2c12bc4bba353fe9e3e61e8ffcca7a5c65e56a
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-12-03 14:57:00 +0100

    service job river

---
+++ src/main/kotlin/no/nav/hm/grunndata/db/servicejob/ServiceJobRiver.kt
@@ -0,0 +1,67 @@
+package no.nav.hm.grunndata.db.servicejob
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.micronaut.context.annotation.Context
+import io.micronaut.context.annotation.Requires
+import kotlinx.coroutines.runBlocking
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.KafkaRapid
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.River
+import no.nav.hm.grunndata.rapid.dto.ServiceJobRapidDTO
+import no.nav.hm.grunndata.rapid.dto.rapidDTOVersion
+import no.nav.hm.grunndata.rapid.event.EventName
+import no.nav.hm.grunndata.rapid.event.RapidApp
+import no.nav.hm.rapids_rivers.micronaut.RiverHead
+import org.slf4j.LoggerFactory
+
+@Context
+@Requires(bean = KafkaRapid::class)
+class ServiceJobRiver(
+    river: RiverHead,
+    private val objectMapper: ObjectMapper,
+    private val serviceJobService: ServiceJobService
+) : River.PacketListener {
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(ServiceJobRiver::class.java)
+    }
+
+    init {
+        river
+            .validate { it.demandValue("createdBy", RapidApp.grunndata_register) }
+            .validate { it.demandValue("eventName", EventName.registeredServiceJobV1) }
+            .validate { it.demandKey("payload") }
+            .validate { it.demandKey("dtoVersion") }
+            .validate { it.demandKey("createdTime") }
+        river.register(this)
+    }
+
+    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        val dtoVersion = packet["dtoVersion"].asLong()
+        if (dtoVersion > rapidDTOVersion) LOG.warn("dto version $dtoVersion is newer than $rapidDTOVersion")
+        val serviceJobRapidDTO = objectMapper.treeToValue(packet["payload"], ServiceJobRapidDTO::class.java)
+        LOG.info("Got service job registration id: ${serviceJobRapidDTO.id} title: ${serviceJobRapidDTO.title}")
+        runBlocking {
+            serviceJobService.saveAndIndex(serviceJobRapidDTO.toEntity())
+        }
+    }
+}
+
+fun ServiceJobRapidDTO.toEntity() = ServiceJob(
+    id = id,
+    title = title,
+    supplierId = supplierId,
+    supplierRef = supplierRef,
+    hmsArtNr = hmsNr,
+    isoCategory = isoCategory,
+    published = published,
+    expired = expired,
+    updated = updated,
+    status = status,
+    created = created,
+    updatedBy = updatedBy,
+    createdBy = createdBy,
+    attributes = attributes,
+    agreements = agreements
+)
--- src/main/kotlin/no/nav/hm/grunndata/db/servicejob/ServiceJobService.kt
+++ src/main/kotlin/no/nav/hm/grunndata/db/servicejob/ServiceJobService.kt
@@ -4,10 +4,13 @@ import io.micronaut.data.model.Page
 import io.micronaut.data.model.Pageable
 import io.micronaut.data.repository.jpa.criteria.PredicateSpecification
 import jakarta.inject.Singleton
+import no.nav.hm.grunndata.db.index.item.IndexItemService
+import no.nav.hm.grunndata.db.index.item.IndexType
+import no.nav.hm.grunndata.db.index.servicejob.toDoc
 import java.util.UUID
 
 @Singleton
-class ServiceJobService(private val repository: ServiceJobRepository) {
+class ServiceJobService(private val repository: ServiceJobRepository, private val indexItemService: IndexItemService) {
 
     suspend fun findServiceJobs(
         buildCriteriaSpec: PredicateSpecification<ServiceJob>? = null,
@@ -18,5 +21,25 @@ class ServiceJobService(private val repository: ServiceJobRepository) {
 
     suspend fun findBySupplierId(supplierId: UUID): List<ServiceJob> =
         repository.findBySupplierId(supplierId)
+
+    suspend fun saveAndIndex(serviceJob: ServiceJob): ServiceJob {
+        val saved = repository.findById(serviceJob.id)?.let {
+            repository.update(it.copy(
+                title = serviceJob.title,
+                supplierRef = serviceJob.supplierRef,
+                hmsArtNr = serviceJob.hmsArtNr,
+                isoCategory = serviceJob.isoCategory,
+                published = serviceJob.published,
+                expired = serviceJob.expired,
+                status = serviceJob.status,
+                attributes = serviceJob.attributes,
+                agreements = serviceJob.agreements,
+                updated = serviceJob.updated,
+                updatedBy = serviceJob.updatedBy
+            ))
+        } ?: repository.save(serviceJob)
+        indexItemService.saveIndexItem(saved.toDoc(), IndexType.SERVICEJOB)
+        return saved
+    }
 
 }
